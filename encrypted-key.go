d18d809c95b811d9df7efddfb9938e90b386d55a
Andrew Stuart <andrew.stuart2@gmail.com>
2016-04-23 20:53:23 -0700

    Add better handling for more algorithms, helpful cert-not-match error message

--- encrypted-key.go
+++ encrypted-key.go
@@ -1,13 +1,16 @@
 package saml
 
 import (
+	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha1"
+	"crypto/sha256"
 	"crypto/tls"
 	"fmt"
+	"hash"
 )
 
 //EncryptedKey contains the decryption key data from the saml2 core and xmlenc
@@ -21,7 +24,13 @@ type EncryptedKey struct {
 
 //EncryptionMethod specifies the type of encryption that was used.
 type EncryptionMethod struct {
-	Algorithm string `xml:"Algorithm,attr"`
+	Algorithm    string `xml:",attr"`
+	DigestMethod DigestMethod
+}
+
+//DigestMethod is a digest type specification
+type DigestMethod struct {
+	Algorithm string `xml:",attr"`
 }
 
 //Well-known encryption methods
@@ -29,8 +38,24 @@ const (
 	MethodRSAOAEP = "http://www.w3.org/2001/04/xmlenc#rsa-oaep-mgf1p"
 )
 
+//Well-known hash methods
+const (
+	MethodSHA1   = "http://www.w3.org/2000/09/xmldsig#sha1"
+	MethodSHA256 = "http://www.w3.org/2000/09/xmldsig#sha256"
+)
+
 //SymmetricKey returns the private key contained in the EncryptedKey document
 func (ek *EncryptedKey) SymmetricKey(cert tls.Certificate) (cipher.Block, error) {
+
+	encCert, err := xmlBytes(ek.X509Data)
+	if err != nil {
+		return nil, fmt.Errorf("error getting certificate from encryptedkey: %v", err)
+	}
+
+	if !bytes.Equal(cert.Certificate[0], encCert) {
+		return nil, fmt.Errorf("key decryption attempted with mismatched cert")
+	}
+
 	cipherText, err := xmlBytes(ek.CipherValue)
 	if err != nil {
 		return nil, err
@@ -38,9 +63,18 @@ func (ek *EncryptedKey) SymmetricKey(cert tls.Certificate) (cipher.Block, error)
 
 	switch pk := cert.PrivateKey.(type) {
 	case *rsa.PrivateKey:
+		var h hash.Hash
+
+		switch ek.EncryptionMethod.DigestMethod.Algorithm {
+		case MethodSHA1:
+			h = sha1.New()
+		case MethodSHA256:
+			h = sha256.New()
+		}
+
 		switch ek.EncryptionMethod.Algorithm {
 		case MethodRSAOAEP:
-			pt, err := rsa.DecryptOAEP(sha1.New(), rand.Reader, pk, cipherText, nil)
+			pt, err := rsa.DecryptOAEP(h, rand.Reader, pk, cipherText, nil)
 			if err != nil {
 				return nil, fmt.Errorf("rsa internal error: %v", err)
 			}
